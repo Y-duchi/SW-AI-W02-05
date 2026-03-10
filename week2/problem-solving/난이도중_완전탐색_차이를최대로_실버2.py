1ace3a75b0dc584c553ca7023712a62e3e6df1be
Y-duchi <du822623@gmail.com>
2026-03-10 16:12:49 +0900

    완전탐색완료

--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,2 +1,28 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
+# 예제 입력 1 
+# 6
+# 20 1 15 8 4 10
+# 예제 출력 1 
+# 62
+
+from itertools import permutations
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+list1 = list(map(int, input().split()))
+
+def function(n, list1):
+    result = [list(p) for p in permutations(list1, n)]
+    answer = []
+    for i in range(0, len(result)):
+        max_number = 0
+        for j in range(0, n-1):
+            max_number += abs(result[i][j] - result[i][j+1])
+        answer.append(max_number)
+
+
+    print(max(answer))
+
+function(n, list1)
