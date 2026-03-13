7a993b87f8d5dc251e650eac70ec8d319f4ae047
Y-duchi <du822623@gmail.com>
2026-03-13 22:45:28 +0900

    07_priority_queue

--- week3/basic/07_priority_queue.py
+++ week3/basic/07_priority_queue.py
@@ -44,12 +44,20 @@ def process_emergency_room(patients):
     pass
         
     processed = []
+
+    for name, priority in patients:
+        heapq.heappush(heap, (priority, name))
     
+
     # TODO: 힙이 비어있지 않은 동안 반복
     ## 힙에서 우선순위가 가장 높은 환자 꺼내기
     ## 환자 처리
     pass
-        
+    while heap :
+        priority, name = heapq.heappop(heap)
+        print(f"처리: {name} (우선순위: {priority})")
+        processed.append(name)
+  
     return processed
 
 # 테스트 케이스
