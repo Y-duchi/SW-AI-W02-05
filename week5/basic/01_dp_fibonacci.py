d26352cedea57d727baf7a636c15b0b7579e77b7
Y-duchi <du822623@gmail.com>
2026-03-27 17:18:13 +0900

    01_dp_fibonacci해결

--- week5/basic/01_dp_fibonacci.py
+++ week5/basic/01_dp_fibonacci.py
@@ -84,18 +84,30 @@ def fibonacci_memo(n, memo=None):
         n번째 피보나치 수
     """
     # TODO: memo가 None이면 빈 딕셔너리로 초기화
-    pass
+    if memo is None:
+        memo = {}
+
+    memo[0] = 0
+    memo[1] = 1
+
+    # 하양식
+    # # TODO: 이미 계산한 값이 memo에 있으면 반환
+    # if n in memo:
+    #     return memo[n]
     
-    # TODO: base case 
-    pass
-    
-    # TODO: 이미 계산한 값이 memo에 있으면 반환
-    pass
-    
-    # TODO: 재귀 호출하여 계산하고 memo에 저장
-    pass
-    
+    # # TODO: 재귀 호출하여 계산하고 memo에 저장
+    # memo[n] = fibonacci_memo(n-1, memo) + fibonacci_memo(n-2, memo)
+
+
+    # 상향식
+    for i in range(2, n+1):
+        memo[i] = memo[i-1] + memo[i-2]
+
+
+
     return memo[n]
+
+
 
 # 테스트 케이스
 if __name__ == "__main__":
